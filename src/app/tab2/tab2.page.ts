1831e0698c4a27e405cc2b8ec304fba3530b2ca1
João Macedo <joaoprbmacedo@gmail.com>
2026-05-21 20:21:02 -0300

    feat: integra consumo de dados da FakeStore no typescript da Tab 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { addIcons } from 'ionicons';
 import { chevronBackOutline, chevronForwardOutline } from 'ionicons/icons';
+import { ApiService } from '../services/api';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonIcon, IonGrid, IonRow, IonCol]
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonIcon, IonGrid, IonRow, IonCol]
 })
+export class Tab2Page implements OnInit {
+  
+  produtos: any[] = [];
 
-export class Tab2Page {
-
-  constructor() {
+  // 2. Injetando o ApiService aqui no construtor!
+  constructor(private apiService: ApiService) {
     addIcons({ chevronBackOutline, chevronForwardOutline });
+  }
+
+  ngOnInit() {
+    this.carregarProdutos();
+  }
+
+  carregarProdutos() {
+    this.apiService.getProdutos().subscribe((dadosDaInternet) => {
+      this.produtos = dadosDaInternet;
+      console.log('Sucesso! Os produtos chegaram:', this.produtos);
+    });
   }
 }
