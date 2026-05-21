dbd418b2a6d8e3c9d791c555425ac4fa919759c0
João Macedo <joaoprbmacedo@gmail.com>
2026-05-21 20:02:04 -0300

    feat: implementa metodo de busca no servico da API

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Api {
-  
+export class ApiService {
+  private urlFakestore = 'https://fakestoreapi.com/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProdutos(): Observable<any> {
+    return this.http.get(this.urlFakestore);
+  }
 }
