83e9c01d5cb908bb2f1c1e013ff3290320770cc8
João Macedo <joaoprbmacedo@gmail.com>
2026-05-21 22:56:30 -0300

    feat: implementa listagem completa do catalogo na tab 3 com recurso de ocultar dados

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton, IonRow, IonCol } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton, IonRow, IonCol],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonRow, IonCol],
 })
 export class Tab1Page {
   constructor() {}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonAvatar, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
+import { ApiService } from '../services/api';
+import { addIcons } from 'ionicons';
+import { trashOutline, downloadOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  standalone: true,
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonAvatar, IonButton, IonButtons, IonIcon]
 })
-export class Tab3Page {
-  constructor() {}
+export class Tab3Page implements OnInit {
+  
+  produtos: any[] = [];
+  mostrarLista: boolean = true;
+
+  constructor(private apiService: ApiService) {
+    addIcons({ trashOutline, downloadOutline });
+  }
+
+  ngOnInit() {
+    this.carregarProdutos();
+  }
+
+  carregarProdutos() {
+    this.apiService.getProdutos().subscribe((dadosDaInternet: any) => {
+      this.produtos = dadosDaInternet;
+      this.mostrarLista = true;
+    });
+  }
+
+  limparDados() {
+    this.mostrarLista = false; 
+  }
 }
